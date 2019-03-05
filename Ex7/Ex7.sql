9ad17ff19bd916fc0afbff93ebda5d534afe92ee
Ram Kaushik R <42760210+RamKaushikR@users.noreply.github.com>
2019-03-05 15:03:10 +0530

    Update Ex7.sql

--- Ex7/Ex7.sql
+++ Ex7/Ex7.sql
@@ -1,3 +1,7 @@
+REM Drop amount from recipts
+alter table receipts 
+drop column amount;
+insert into products values('50-klmop','almond','twist',6.12);
 REM 1  The combination of Flavor and Food determines the product id. Hence, while 
 REM    inserting a new instance into the Products relation, ensure that the same combination
 REM    of Flavor and Food is not already available.
@@ -26,7 +30,7 @@ insert into products values('50-klmop','almond','twist',6.12);
 REM 2. While entering an item into the item_list relation, update the amount in Receipts with
 REM the total amount for that receipt number.
 alter table receipts
-add amount float;
+add amount float default 0;
 create view tot_price as
 select i.rno as rno,sum(p.price) as price from item_list i join products p 
 on i.item=p.pid group by i.rno;
@@ -34,18 +38,24 @@ create view temp_view as
 select r.rno as rno,r.rdate as rdate,r.cid as cid,t.price as amount
 from receipts r left join tot_price t
 on r.rno=t.rno;
+update receipts r
+set amount=(select amount from temp_view
+where rno=r.rno);
 create or replace trigger q2
-after insert on item_list for each row 
+before insert on item_list for each row 
 declare
 	price products.price%type;
 begin
 	select p.price into price
 	from products p where p.pid=:new.item;
-	update temp_view
+	update receipts
 	set amount=amount+price
 	where :new.rno=rno;
 end;
-/		
+/
+select * from receipts where rno=46674;
+insert into item_list values(46674,2,'70-R');
+select * from receipts where rno=46674;
 REM 3 Implement the following constraints for Item_list relation:
 REM a. A receipt can contain a maximum of five items only.
 REM b. A receipt should not allow an item to be purchased more than thrice.
@@ -81,3 +91,5 @@ begin
 	end loop;
 end;
 /
+insert into item_list values(41028, 2,  '90-BER-11');
+insert into item_list values(41028, 3,  '90-BER-11');
